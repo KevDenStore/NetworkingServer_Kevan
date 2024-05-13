9704a6c478448dead4a4a9efd4bd698f84926ecf
KevDenStore <120115245+KevDenStore@users.noreply.github.com>
2024-05-13 09:17:08 +0200

    andra

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,11 @@
-SAD
+Import java.net. *;
+Import java.io. *;
+
+public class NetworkingServer {
+    public static void main(String [] args)
+
+        ServerSocket server = null;
+        Socket client;
+
+        int portnumber =
+}
